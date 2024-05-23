42a77346dd7cb0e6c24170c1d663a4944dea3de2
nogueira <marcosrafael.nm62@gmail.com>
2024-05-22 21:11:52 -0300

    avanços no backend

--- Site/Backend/controllers/peçasController.js
+++ Site/Backend/controllers/peçasController.js
@@ -1,56 +1,112 @@
-const {Peças} = require('../models');
+const { Peças } = require("../models");
 
-const findAllPeças = async(req,res) =>{
-    try{
-        peças = await Peças.findAll();
-        if(!peças){
-            return res.status(404).send({
-                message: "Sem peças cadastradas"
-            })
-        }
-        return res.status(200).send({
-            message: "Requisição com sucesso!",
-            peças: peças
-        })
-    }catch(error){
-        console.error("Erro durante a consulta nos dados, erro: ", error);
-        return res.status(500).send({
-            message: "Erro durante a consulta de dados",
-            error: error
-        })
-     
+const findAllPeças = async (req, res) => {
+  try {
+    console.log("aqui");
+    peças = await Peças.findAll();
+    if (!peças) {
+      return res.status(404).send({
+        message: "Sem peças cadastradas",
+      });
     }
-}
+    return res.status(200).send({
+      message: "Requisição com sucesso!",
+      peças: peças,
+    });
+  } catch (error) {
+    console.error("Erro durante a consulta nos dados, erro: ", error);
+    return res.status(500).send({
+      message: "Erro durante a consulta de dados",
+      error: error,
+    });
+  }
+};
 
-const createPeça = async(req, res) =>{
-    try{
-        const request = req.body
-        console.log(req.body)
-        //console.log(req.body.tipo)
-        console.log(request)
-        const {nome, tipo, preço} = request
-        if(!nome || !tipo ||  !preço){
-            return res.status(400).send({message: "Todos os campos são obrigatórios."})
-        }
-        await Peças.create(request);
-        return res.status(200).send({
-            message: "Peça registrada com sucesso!",
-            request: request
-        })
-        
-
-    }catch(error){
-        console.error("Erro durante a a criação, erro: ", error);
-        return res.status(500).send({
-            message: "Erro interno de servidor",
-            error: error
-        
-        })
-        
+const findPerName = async (req, res) => {
+  try {
+    const nome = req.query.nome;
+    console.log(nome);
+    if (!nome) {
+      return res.status(400).send({
+        message: "O campo nome é obrigatório",
+      });
     }
-}
 
-module.exports ={
-    findAllPeças,
-    createPeça
-}
+    const peça = await Peças.findAll({ where: { nome: nome } });
+
+    if (!peça) {
+      return res.status(400).send({
+        message: "Não existem peças cadastradas com esse nome",
+      });
+    }
+    return res.status(200).send({
+      message: "Sucesso!",
+      peça: peça,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Não foi possível concluir a requisição",
+      error: error,
+    });
+  }
+};
+
+const findByPk = async (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log(id);
+    if (!id) {
+      return res.status(400).send({
+        message: "O campo ID é obrigatório!",
+      });
+    }
+    const peça = await Peças.findByPk(id);
+    if (!peça) {
+      return res.status(400).send({
+        message: "Não existem peças cadastradas com esse ID",
+      });
+    }
+    return res.status(200).send({
+      message: "Sucesso!",
+      peça: peça,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Não foi possível concluir a requisição",
+      erro: error,
+    });
+  }
+};
+
+const createPeça = async (req, res) => {
+  try {
+    const request = req.body;
+    console.log(req.body);
+    //console.log(req.body.tipo)
+    console.log(request);
+    const { nome, tipo, preço } = request;
+    if (!nome || !tipo || !preço) {
+      return res
+        .status(400)
+        .send({ message: "Todos os campos são obrigatórios." });
+    }
+    await Peças.create(request);
+    return res.status(200).send({
+      message: "Peça registrada com sucesso!",
+      request: request,
+    });
+  } catch (error) {
+    console.error("Erro durante a a criação, erro: ", error);
+    return res.status(500).send({
+      message: "Erro interno de servidor",
+      error: error,
+    });
+  }
+};
+
+module.exports = {
+  findAllPeças,
+  createPeça,
+  findPerName,
+  findByPk,
+};
--- Site/Backend/routers/peçasRouter.js
+++ Site/Backend/routers/peçasRouter.js
@@ -5,6 +5,8 @@ const peçasController = require('../controllers/peçasController')
 
 router.get("/", peçasController.findAllPeças);
 router.post("/", peçasController.createPeça);
+router.get("/search/", peçasController.findPerName)
+router.get("/:id", peçasController.findByPk);
 
 
 module.exports = router;
